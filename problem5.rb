8eafdd854238dbd0b6d9c3aad09f7afade5ad67d
ta-chibana <takuma.chibana314@gmail.com>
2015-11-01 18:59:11 +0900

    fix max value MAX_VALUEを問題文通りに修正

--- problem5.rb
+++ problem5.rb
@@ -1,10 +1,10 @@
 # 2520 は 1 から 10 の数字の全ての整数で割り切れる数字であり, そのような数字の中では最小の値である.
 # では, 1 から 20 までの整数全てで割り切れる数字の中で最小の正の数はいくらになるか.
 
-MAX_NUMBER = 50
+MAX_VALUE = 20
 
 def calc
-  (2..MAX_NUMBER).reduce { |result, num| result *= factory(result, num) }
+  (2..MAX_VALUE).reduce { |result, num| result *= factory(result, num) }
 end
 
 def factory(result, next_num)
