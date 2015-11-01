d76dd96dfed4a678f2d4d730303dccb0ebf6547c
ta-chibana <takuma.chibana314@gmail.com>
2015-11-01 20:46:50 +0900

    eachをreduceに変更

--- problem6.rb
+++ problem6.rb
@@ -1,11 +1,9 @@
 # 最初の100個の自然数について二乗の和と和の二乗の差を求めよ.
 
 def calc &block
-  result = 0
-  (1..100).each do |num|
+  (1..100).reduce do |result, num|
     result += block.call(num)
   end
-  result
 end
 
 p (calc { |arg| arg } ** 2) - (calc { |arg| arg ** 2 })
